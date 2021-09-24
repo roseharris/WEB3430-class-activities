7003e9f3353fe41ac752441a8bc5728c94c7761d
Rose Harris <roseharris801@gmail.com>
2021-09-23 22:56:26 -0600

    9/23 upload

--- src/javascripts/components/StarRating.js
+++ src/javascripts/components/StarRating.js
@@ -1,22 +1,20 @@
 import React from "react";
 import { FaStar } from "react-icons/fa";
-//import { top10 } from "../top10";
 
-
-const Star = ({ selected }) => (
-  <FaStar color={selected ? "red" : "grey"}/>
+const Star = ({ selected }) => ( //pass in selected value
+  <FaStar color={selected ? "red" : "grey"}/> //If true color star maroon, if false grey
 );
 
 const createArray = (length) => [...Array(length)];
 
 export default function StarRating(props) { //destructuring
-    const rating = props.rating
+    const rating = props.rating;//assigning the passed in rating to a variable to use in the function
     return (
       <>
-        {createArray(5).map((n, i) => (
+        {createArray(5).map((n, i) => ( //create the star array using map function
           <Star
             key={i}
-            selected ={i < (Math.floor(rating / 2) - 1)} //Am I creating the star array correctly?
+            selected ={i < Math.floor(rating / 2) - 1} //Am I creating the star array correctly?
           />
         ))}
       </>
